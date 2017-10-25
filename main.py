678b6f4adc0a44310e6d6a66f198a5bbf7445c76
Katreque <renanverissimo@uol.com.br>
2017-10-25 18:09:35 -0200

    Captura e limpeza das screenshots em loop adicionadas.

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import cv2
+import numpy as np
 from PIL import ImageGrab
+from time import sleep
 
+def ajustaImg():
+  img = cv2.imread('screenshot.png')
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+  kernel = np.ones((1, 1), np.uint8)
+  img = cv2.dilate(img, kernel, iterations=1)
+  img = cv2.erode(img, kernel, iterations=1)
+  img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+
+  cv2.imwrite("screenshot.png", img)
 
 def tirarPrint():
-  img = ImageGrab.grab()
-  return img.save('screenshot.png')
+  while(1):
+    img = ImageGrab.grab()
+    img.save('screenshot.png')
+    ajustaImg()
+    sleep(0.03)
+
 
 
 tirarPrint()
