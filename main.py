3c56886efba98e87279f251de5ba2c38062f9f30
Katreque <renanverissimo@uol.com.br>
2017-10-27 17:05:49 -0200

    Crop and space press added.

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import cv2
 import numpy as np
-from PIL import ImageGrab
+from PIL import ImageGrab, Image
 from time import sleep
+import pypiwin32
+
+def pular():
+  shell = win32com.client.Dispatch("WScript.Shell")
+  shell.SendKeys("{SPACE}")
 
 def ajustaImg():
+  img = Image.open('screenshot.png').convert('L')
+  img = img.resize((1280, 720), Image.ANTIALIAS)
+  img = img.crop((265, 345, 285, 365))
+  #img = img.crop((165, 245, 385, 465))
+  img.save('screenshot.png')
+
   img = cv2.imread('screenshot.png')
   img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -15,7 +26,7 @@ def ajustaImg():
   cv2.imwrite("screenshot.png", img)
 
 def tirarPrint():
-  while(1):
+  #while(1):
     img = ImageGrab.grab()
     img.save('screenshot.png')
     ajustaImg()
