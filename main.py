9e32d69a8bcef048efd3c0e984c419cc3c30ac99
Katreque <renanverissimo@uol.com.br>
2017-11-01 15:29:26 -0200

    Jump function added + minor fixes

--- main.py
+++ main.py
@@ -1,37 +1,33 @@
 import cv2
 import numpy as np
 from PIL import ImageGrab, Image
-from time import sleep
-import pypiwin32
+import SendKeys
 
 def pular():
-  shell = win32com.client.Dispatch("WScript.Shell")
-  shell.SendKeys("{SPACE}")
+  send = "{SPACE}"
+  SendKeys.SendKeys(send)
 
 def ajustaImg():
-  img = Image.open('screenshot.png').convert('L')
-  img = img.resize((1280, 720), Image.ANTIALIAS)
+  img = Image.open('S.png').convert('L')
   img = img.crop((265, 345, 285, 365))
-  #img = img.crop((165, 245, 385, 465))
-  img.save('screenshot.png')
+  img.save('S.png')
 
-  img = cv2.imread('screenshot.png')
+  img = cv2.imread('S.png')
   img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  cv2.imwrite("S.png", img)
 
-  kernel = np.ones((1, 1), np.uint8)
-  img = cv2.dilate(img, kernel, iterations=1)
-  img = cv2.erode(img, kernel, iterations=1)
-  img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+  img = Image.open('S.png').convert('L')
+  if not img.getbbox():
+    print('Yep!')
+    pular()
+  else:
+    print('Noup!')
 
-  cv2.imwrite("screenshot.png", img)
 
 def tirarPrint():
-  #while(1):
+  while(1):
     img = ImageGrab.grab()
-    img.save('screenshot.png')
+    img.save('S.png')
     ajustaImg()
-    sleep(0.03)
-
-
 
 tirarPrint()
