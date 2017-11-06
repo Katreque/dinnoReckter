b3b887fa1c000b6142ad3d0678d65ba45e719070
Katreque <renanverissimo@uol.com.br>
2017-11-06 14:34:39 -0200

    Ajuste no crop de detecção para jump

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ def pular():
 
 def ajustaImg():
   img = ImageGrab.grab()
-  img = img.crop((463, 540, 468, 545))
-  #img = img.crop((410, 487, 520, 597))
 
   imgcv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
   imgcv = cv2.cvtColor(imgcv, cv2.COLOR_BGR2GRAY)
   _,imgcv = cv2.threshold(imgcv, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-  cv2.imwrite("s.png", imgcv)
 
-  if cv2.countNonZero(imgcv) < 15:
+  crop_img = imgcv[485:495, 575:585]
+  cv2.imwrite("s.png", crop_img)
+
+  if cv2.countNonZero(crop_img) < -1:
     print("Jump")
     pular()
 
